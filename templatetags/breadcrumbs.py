de08d0f51bb65788717d8a79ebf99d91948a3ad1
Stanislav Baltrunas <stanislav@baltrunas.ru>
2015-07-19 22:24:38 +0600

    Add breadcrumbs template tag

---
+++ templatetags/breadcrumbs.py
@@ -0,0 +1,20 @@
+from django import template
+register = template.Library()
+
+
+def get_br(obj):
+	br = [obj]
+	if hasattr(obj, 'parent') and obj.parent:
+		br += get_br(obj.parent)
+	elif hasattr(obj, 'category') and obj.category:
+		br += get_br(obj.category)
+	return br
+
+@register.simple_tag(takes_context=True)
+def breadcrumbs(context, obj):
+	context = {}
+	context['breadcrumbs'] = reversed(get_br(obj))
+
+	tpl = template.loader.get_template('useful/breadcrumbs.html')
+	return tpl.render(template.Context(context))
+
