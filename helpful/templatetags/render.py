98d6670ca1afbfa8231a934f2e8eb4faee370a79
Stanislav Baltrunas <stanislav@baltrunas.ru>
2017-03-17 12:51:03 +0600

    debug

--- helpful/templatetags/render.py
+++ helpful/templatetags/render.py
@@ -1,5 +1,6 @@
 from django import template
 from django.utils.safestring import mark_safe
+from django.conf import settings
 
 
 register = template.Library()
@@ -7,9 +8,14 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def render(context, content):
-	try:
+	if settings.DEBUG:
 		tpl = template.Template(content)
 		content = template.Context(context)
 		return tpl.render(content)
-	except Exception as e:
-		return mark_safe("<div cllass='b-error'>Render Error: <code>%s</code></div>" % e.message)
+	else:
+		try:
+			tpl = template.Template(content)
+			content = template.Context(context)
+			return tpl.render(content)
+		except Exception as e:
+			return mark_safe("<div cllass='b-error'>Render Error: <code>%s</code></div>" % e.message)
